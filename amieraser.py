dfc24b2f9004f28c1dfc9cd23bd91e2b1bc1e522
Devin Boyer <drb5272@gmail.com>
2016-10-26 14:03:39 -0400

    Take in multiple image ids at once

--- amieraser.py
+++ amieraser.py
@@ -30,21 +30,24 @@ if options.id == None:
 session = boto3.Session(profile_name=aws_profile)
 client = session.client('ec2', region_name=aws_region)
 
-try:
-    response = client.describe_images(ImageIds=[options.id])
-    devices = response['Images'][0]['BlockDeviceMappings']
-    snap_ids = [ device['Ebs']['SnapshotId'] for device in devices ]
-    
+image_ids = options.id.split(',')
 
-    # Delete the image itself
-    client.deregister_image(ImageId=options.id)
-    print("Deregistered %s" % options.id)
+for image_id in image_ids:
+    try:
+        response = client.describe_images(ImageIds=[image_id])
+        devices = response['Images'][0]['BlockDeviceMappings']
+        snap_ids = [ device['Ebs']['SnapshotId'] for device in devices ]
+        
 
-    # Delete any associated snapshots
-    for snap_id in snap_ids:
-        client.delete_snapshot(SnapshotId=snap_id)
-        print("Deleted snapshot %s" % snap_id)
-except botocore.exceptions.ClientError as e:
-    print("An error occured deleting the image %s:" % options.id)
-    print("\t" + str(e))
-    sys.exit(1)
+        # Delete the image itself
+        client.deregister_image(ImageId=image_id)
+        print("Deregistered %s" % image_id)
+
+        # Delete any associated snapshots
+        for snap_id in snap_ids:
+            client.delete_snapshot(SnapshotId=snap_id)
+            print("Deleted snapshot %s" % snap_id)
+    except botocore.exceptions.ClientError as e:
+        print("An error occured deleting the image %s:" % image_id)
+        print("\t" + str(e))
+        sys.exit(1)
